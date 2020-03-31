72b3d58b9f31051ff9b9d33fa35fb991970cdd63
chenf24k <chenf24k@163.com>
2020-03-31 23:57:07 +0800

    react-style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import './index.css'
+import classNames from 'classnames';
+import styled from 'styled-components'
 
-ReactDOM.render(
-  <React.StrictMode>
+const Title = styled.li`
+    color: red
+`
+
+class App extends Component {
+    render() {
+        console.log(this.props);
+
+        const style = { color: 'red' }
+        return (
+            <div>
+                <h1 style={style}>JSX原理</h1>
+                <ol>
+                    <li style={style}>使用style内联创建</li>
+                    <li className="has-text-red">使用class的方式</li>
+                    <li className={classNames('a', { 'b': true, 'c': false })}>使用第三方依赖动态添加样式</li>
+
+                    <Title>styled-components的使用</Title>
+                </ol>
+            </div>
+        )
+    }
+}
+
+
+render(
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    ,
+    document.getElementById('root')
+)
